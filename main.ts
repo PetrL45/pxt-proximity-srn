cd1735cbc1e7e543af6dc7defc5f4ebf78b9ee36
PetrL45 <146720250+PetrL45@users.noreply.github.com>
2024-01-29 13:18:04 +0100

    save1

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+let prumer: Number;
+let lightLevel = input.lightLevel();
+let tries = 0;
+basic.forever(function() {
+    console.log(input.lightLevel())
+    basic.pause(50)
+    if(lightLevel > prumer + 10 || lightLevel < prumer - 10){
+        music.playTone(300, 500)
+    }
+})
+input.onButtonPressed(Button.A, function() {
+    tries += 1
+    console.log(input.lightLevel())
+    for(tries; tries = 5; tries + )
+    basic.pause(50)
+})
